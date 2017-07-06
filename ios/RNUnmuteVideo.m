edc71032c9e6b7da9810091dbca8e15d8a679308
Rafael Nascimento <rafael.nascimento@possible.com>
2017-07-06 10:27:53 +0200

    Add category ambient

--- ios/RNUnmuteVideo.m
+++ ios/RNUnmuteVideo.m
@@ -11,22 +11,30 @@
 }
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(listenToButton) {
+RCT_EXPORT_METHOD(turnSound:(NSString*)param) {
     AVAudioSession* audioSession = [AVAudioSession sharedInstance];
     [audioSession setActive:YES error:nil];
     [audioSession addObserver:self
-                   forKeyPath:@"outputVolume"
+                   forKeyPath:param
                       options:0
                       context:nil];
 }
 
 -(void) observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
     
-    if ([keyPath isEqual:@"outputVolume"]) {
-        NSLog(@"volume changed");
+    if ([keyPath isEqual:@"outputVolumeOn"]) {
+        NSLog(@"volume changed: on");
         NSError *setCategoryError = nil;
         BOOL success = [[AVAudioSession sharedInstance]
                         setCategory: AVAudioSessionCategoryPlayback
+                        error: &setCategoryError];
+        
+        if (!success) { /* handle the error in setCategoryError */ }
+    } else if ([keyPath isEqual:@"outputVolumeOff"]) {
+        NSLog(@"volume changed: off");
+        NSError *setCategoryError = nil;
+        BOOL success = [[AVAudioSession sharedInstance]
+                        setCategory: AVAudioSessionCategoryAmbient
                         error: &setCategoryError];
         
         if (!success) { /* handle the error in setCategoryError */ }
@@ -34,4 +42,3 @@ RCT_EXPORT_METHOD(listenToButton) {
 }
 
 @end
-  
