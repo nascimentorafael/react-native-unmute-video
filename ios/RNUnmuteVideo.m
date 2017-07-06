da0918e095d76e4bf79b38106ea8178d76d89d68
Rafael Nascimento <rafael.nascimento@possible.com>
2017-07-06 10:37:22 +0200

    Add category ambient2

--- ios/RNUnmuteVideo.m
+++ ios/RNUnmuteVideo.m
@@ -11,34 +11,44 @@
 }
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(turnSound:(NSString*)param) {
+RCT_EXPORT_METHOD(turnSoundOn) {
     AVAudioSession* audioSession = [AVAudioSession sharedInstance];
     [audioSession setActive:YES error:nil];
     [audioSession addObserver:self
-                   forKeyPath:param
+                   forKeyPath:@"outputVolumeUp"
+                      options:0
+                      context:nil];
+}
+
+RCT_EXPORT_METHOD(turnSoundOff) {
+    AVAudioSession* audioSession = [AVAudioSession sharedInstance];
+    [audioSession setActive:YES error:nil];
+    [audioSession addObserver:self
+                   forKeyPath:@"outputVolumeDown"
                       options:0
                       context:nil];
 }
 
 -(void) observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
     
-    if ([keyPath isEqual:@"outputVolumeOn"]) {
-        NSLog(@"volume changed: on");
+    if ([keyPath isEqual:@"outputVolumeUp"]) {
+        NSLog(@"volume changed: up");
         NSError *setCategoryError = nil;
         BOOL success = [[AVAudioSession sharedInstance]
                         setCategory: AVAudioSessionCategoryPlayback
                         error: &setCategoryError];
         
         if (!success) { /* handle the error in setCategoryError */ }
-    } else if ([keyPath isEqual:@"outputVolumeOff"]) {
-        NSLog(@"volume changed: off");
+    } else if ([keyPath isEqual:@"outputVolumeDown"]){
+        NSLog(@"volume changed: down");
         NSError *setCategoryError = nil;
         BOOL success = [[AVAudioSession sharedInstance]
                         setCategory: AVAudioSessionCategoryAmbient
                         error: &setCategoryError];
         
-        if (!success) { /* handle the error in setCategoryError */ }
+        if (!success) { /* handle the error in setCategoryError *
     }
 }
 
 @end
+  
